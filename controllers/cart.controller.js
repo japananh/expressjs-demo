aa51b4b5a667d8222cbbd7724fe183cc25e2c588
nhat-anh <japananh@gmail.com>
2019-03-27 17:44:47 +0700

    Add quantity of user cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -9,15 +9,27 @@ module.exports.addToCart = (req, res, next) => {
     return;
   }
 
-  let count = db
+  let userSession = db
     .get('sessions')
-    .find({ id: sessionId })
+    .find({ id: sessionId });
+
+  let count = userSession
     .get('cart.' + productId, 0)
     .value();
 
-  db.get('sessions')
-    .find({ id: sessionId })
-    .set('cart.' + productId, count + 1)
+  userSession.set('cart.' + productId, count + 1)
+    .write();
+
+  let userCart = userSession
+    .value()['cart'];
+
+  let quantity = 0;
+  
+  for (key in userCart) {
+    quantity += key !== 'quantity' ? userCart[key] : 0;
+  }
+  
+  userSession.set('cart.quantity', quantity)
     .write();
 
   res.redirect('/products');
